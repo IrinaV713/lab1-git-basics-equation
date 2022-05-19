6c3b5853098d75ce42f55f09dce2bfef2503a165
Irina Velychko <76870532+IrinaV713@users.noreply.github.com>
2022-05-19 13:29:27 +0300

    Revert "Update NonInteractive.java"
    This reverts commit b2ac9e3874f44d36b468dd5dc9a182cbc5146146.

--- equation/src/com/company/NonInteractive.java
+++ equation/src/com/company/NonInteractive.java
@@ -1,43 +1,5 @@
 package com.company;
-import java.io.*;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class NonInteractive {
-    public void nonInteractive() {
-        String data = "";
-        System.out.println("Enter the name and path of your file: ");
-        Scanner scan = new Scanner(System.in);
-        String fpath = scan.nextLine();
-
-        //checking file existence and content; reading data from file
-        try {
-            FileReader file = new FileReader(fpath);
-            BufferedReader buffer = new BufferedReader(file);
-            String line = buffer.readLine();
-            Pattern pattern = Pattern.compile("((?:\\d+\\s){2}\\d+)");
-            Matcher matcher = pattern.matcher(line);
-            if (matcher.find()) {
-                data += line;
-            } else {
-                System.out.println("Error. Invalid file content");
-                System.exit(1);
-            }
-        } catch (IOException e) {
-            System.out.println("Error. IO exception occurred. File may not exist or path/name is invalid");
-            System.exit(1);
-        }
-
-        //output the whole equation
-        String[] strc = data.split(" ");
-        double a = Double.parseDouble(strc[0]);
-        double b = Double.parseDouble(strc[1]);
-        double c = Double.parseDouble(strc[2]);
-        System.out.print("Equation is: ");
-        System.out.println("(" + a + ") " + "x^2 " + "+ " + "(" + b + ") " + "x " + "+ " + "(" + c + ") " + "= " + "0");
-
-        //calculating roots
-        Interactive.getRoots(a, b, c);
-    }
+    
 }
