35719f86757127edc406762d207482e5353b4381
Irina Velychko <76870532+IrinaV713@users.noreply.github.com>
2022-05-19 12:59:36 +0300

    Add app modes to Main.java

--- equation/src/com/company/Main.java
+++ equation/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Select an application mode.");
+        while(true){
+            System.out.print("Type 0 for interactive mode; type 1 for file mode:");
+            Scanner main_scan = new Scanner(System.in);
+            String mode = main_scan.nextLine();
+            if(Objects.equals(mode, "0")){
+                //Interactive obj1 = new Interactive();
+                //obj1.interactive();
+                break;
+            }
+            else if(Objects.equals(mode, "1")){
+                //NonInteractive obj2 = new NonInteractive();
+                //obj2.nonInteractive();
+                break;
+            }
+            else System.out.println("Error. Expected 0 or 1");
+        }
     }
 }
